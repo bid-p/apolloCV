0b7ad76c9ca4a90c6f45c27fbc0b50cfaf60382c
Vinodh N <vinodhnagarajan@outlook.com>
2019-02-19 18:56:54 -0800

    Cleanup REEEE
    Pls work

--- .vscode/cquery_cached_index/c@@robotics@apollo/include@drive.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "okapi/api.hpp"
-
-extern okapi::Controller controller;
-extern okapi::Motor driveR1, driveR2, driveL1, driveL2;
-extern char getDriveState();
-extern void abortDrive();
-extern void updateDrive();
-extern void driveAct();
